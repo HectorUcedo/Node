f0e71a6df4292319b59dbe9a7802662d38937b99
heucedo <hector.ucedo@itti.digital>
2022-10-05 22:03:44 -0300

    10- Ciclo de vida de un proceso Node

--- 01-hola-mundo/app.js
+++ 01-hola-mundo/app.js
@@ -1,11 +1,3 @@
 
 
-//let nombre = 'Hector';
-const nombre = 'Hector';
-console.log(nombre);
-
-//nombre = 'Joy';
-const nombre2 = 'Joy';
-console.log(nombre2);
-
-console.log('Hola mundo!');
+onsole.log('Hola mundo!');
---
+++ 01-hola-mundo/app2.js
@@ -0,0 +1,10 @@
+
+
+// va a recibir un Argumento nombre
+const saludar = ( nombre ) => {
+
+    // Alt +
+    return `Saludos ${ nombre }`; // 'Saludos ' + nombre;
+}
+
+console.log( saludar('Héctor') );
