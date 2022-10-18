2fb7f66c91bf4a4ccb2cd7d9e929f9f73bd74abc
heucedo <hector.ucedo@itti.digital>
2022-10-18 16:36:11 -0300

    34. Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,16 +2,20 @@
  
 
 const { fug_crearArchivo } = require('./helpers/multiplicar'); // Desestructuración
+const argv = require('yargs').argv;
 
 console.clear();
 
+console.log( process.argv );
+console.log( argv );
+console.log( 'base: yargs', argv.base );
 
 
 //console.log(process.argv);
 
 // Destructuración de arreglos:
-const [ , , arg3 = 'base=5' ] = process.argv;
-const [ , base = 5 ] = arg3.split('=');
+//* const [ , , arg3 = 'base=5' ] = process.argv;
+//* const [ , base = 5 ] = arg3.split('=');
 //console.log(arg3);
 //console.log(base);
 
@@ -19,6 +23,6 @@ const [ , base = 5 ] = arg3.split('=');
 
 // const base =5;
 
-fug_crearArchivo( base )
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch( err => console.log(err) );
+// fug_crearArchivo( base )
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch( err => console.log(err) );
